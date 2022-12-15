68c16ffb41df9ffd1b9c5158c5d2e1bcfd61dc10
Jay Welsh <thealchemicalopus@gmail.com>
2022-12-15 04:46:10 +0200

    misc(GithubRepoNavigatorDialog): block selection design

--- src/components/GithubRepoNavigatorDialog.tsx
+++ src/components/GithubRepoNavigatorDialog.tsx
@@ -14,16 +14,30 @@ interface IGithubRepoNavigatorProps {
   onClose: () => void;
 }
 
-const RepoEntry = styled('div')(({ theme }) => ({
-  textAlign: 'center',
-  padding: theme.spacing(2),
-}));
-
 const RepoEntryContainer = styled('div')(({ theme }) => ({
   margin: theme.spacing(2),
   marginTop: 0,
 }));
 
+const RepoEntrySquareOuter = styled('div')(({ theme }) => ({
+  position: 'relative',
+  borderRadius: '5px',
+  paddingTop: '100%',
+}));
+
+const RepoEntrySquareInner = styled('div')(({ theme }) => ({
+  position: 'absolute',
+  top: '50%',
+  left: '50%',
+  transform: 'translateX(-50%)translateY(-50%)',
+  width: '100%',
+  height: '100%',
+}));
+
+const RepoEntry = styled('div')(({ theme }) => ({
+  textAlign: 'center',
+  padding: theme.spacing(2),
+}));
 
 const GithubRepoNavigatorDialog = (props: IGithubRepoNavigatorProps) => {
   const { onClose, open } = props;
@@ -38,24 +52,32 @@ const GithubRepoNavigatorDialog = (props: IGithubRepoNavigatorProps) => {
       <RepoEntryContainer>
         <Grid container spacing={2}>
           <Grid item xs={12} md={6}>
-            <ExternalLink href="https://github.com/JayWelsh/silo-observer-frontend">
-              <CardActionArea className="hover-opacity-button" style={{borderRadius: 5}}>
-                <RepoEntry>
-                  <PreviewIcon style={{width: 120, height: 120}}/>
-                  <Typography variant="h6">Frontend</Typography>
-                </RepoEntry>
-              </CardActionArea>
-            </ExternalLink>
+            <RepoEntrySquareOuter>
+              <RepoEntrySquareInner>
+                <ExternalLink href="https://github.com/JayWelsh/silo-observer-frontend">
+                  <CardActionArea className="hover-opacity-button full-height hover-border" style={{borderRadius: 5}}>
+                    <RepoEntry>
+                      <PreviewIcon style={{width: 120, height: 120, marginBottom: 12}}/>
+                      <Typography variant="h6">Frontend</Typography>
+                    </RepoEntry>
+                  </CardActionArea>
+                </ExternalLink>
+              </RepoEntrySquareInner>
+            </RepoEntrySquareOuter>
           </Grid>
           <Grid item xs={12} md={6}>
-            <ExternalLink href="https://github.com/JayWelsh/silo-observer-backend">
-              <CardActionArea className="hover-opacity-button" style={{borderRadius: 5}}>
-                <RepoEntry>
-                  <SettingsSuggestIcon style={{width: 120, height: 120}}/>
-                  <Typography variant="h6">Backend</Typography>
-                </RepoEntry>
-              </CardActionArea>
-            </ExternalLink>
+            <RepoEntrySquareOuter>
+              <RepoEntrySquareInner>
+                <ExternalLink href="https://github.com/JayWelsh/silo-observer-backend">
+                  <CardActionArea className="hover-opacity-button full-height hover-border" style={{borderRadius: 5}}>
+                    <RepoEntry>
+                      <SettingsSuggestIcon style={{width: 120, height: 120, marginBottom: 12}}/>
+                      <Typography variant="h6">Backend</Typography>
+                    </RepoEntry>
+                  </CardActionArea>
+                </ExternalLink>
+              </RepoEntrySquareInner>
+            </RepoEntrySquareOuter>
           </Grid>
         </Grid>
       </RepoEntryContainer>
