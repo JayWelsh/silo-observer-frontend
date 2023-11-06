f13833e9c05f6e611322c7d48f75752b3da35d75
Jay Welsh <thealchemicalopus@gmail.com>
2023-11-06 12:56:38 +0200

    feat(UserMetricsChartSelection): enable chain filtering

--- src/containers/UserMetricsChartSelectionContainer.tsx
+++ src/containers/UserMetricsChartSelectionContainer.tsx
@@ -5,11 +5,13 @@ import UserMetricsChartSelection from '../components/UserMetricsChartSelection';
 interface RootState {
   darkMode: boolean
   isConsideredMobile: boolean
+  selectedNetworkIDs: string[]
 }
   
 const mapStateToProps = (state: RootState) => ({
   darkMode: state.darkMode,
   isConsideredMobile: state.isConsideredMobile,
+  selectedNetworkIDs: state.selectedNetworkIDs,
 })
 
 const connector = connect(mapStateToProps, {})
